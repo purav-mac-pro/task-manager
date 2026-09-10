7809c82421acd5a7cc869e1a1323173262b02b9a
Puravxgrover <Puravxgrover@gmail.com>
2026-09-10 12:44:44 +0530

    Added urls

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Origin',
-    'http://localhost:5173'
+    'https://task-manager-client-ehri.onrender.com'
   );
 
   res.header(
